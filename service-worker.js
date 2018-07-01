c308d75f423b22ef591ab431ab504d5c8d0d7b30
Samuel  Achema <achemaku@gmail.com>
2018-07-01 18:37:53 +0100

    Updated
    Updated

--- service-worker.js
+++ service-worker.js
@@ -9,47 +9,38 @@ var filesToCache = [
   '/favicon.ico'
 ];
 
-self.addEventListener('install', function(e) {
+
+self.addEventListener('install', e => {
   console.log('[ServiceWorker] Install');
   e.waitUntil(
-    caches.open(cacheName).then(function(cache) {
+    caches.open(cacheName).then(cache => {
       console.log('[ServiceWorker] Caching app shell');
       return cache.addAll(filesToCache);
-    })
+    }),
   );
 });
 
-self.addEventListener('activate', function(e) {
+
+self.addEventListener('activate', e => {
   console.log('[ServiceWorker] Activate');
   e.waitUntil(
-    caches.keys().then(function(keyList) {
-      return Promise.all(keyList.map(function(key) {
-        if (key !== cacheName) {
-          console.log('[ServiceWorker] Removing old cache', key);
-          return caches.delete(key);
-        }
-      }));
-    })
-  );});
+    caches.keys().then(keyList =>
+      Promise.all(
+        keyList.map(key => {
+          if (key !== cacheName) {
+            return caches.delete(key);
+          }
+        }),
+      ),
+    ),
+  );
+});
 
-/**
- * Serve app from cache if there is a cached version
- */
-self.addEventListener('fetch', function(event){    
-    console.log('[Service Worker] Fetch', event.request.url);
-    var requestUrl = new URL(event.request.url);
 
-    if (requestUrl.origin === location.origin) {
-        if (requestUrl.pathname === '/') {
-            event.respondWith(caches.match('/'));
-            return;
-
-        }
-    }
-    event.respondWith(
-        caches.match(event.request).then(function(response) {
-          if (response) return response;
-          return fetch(event.request);        
-        })        
-      );
- });
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches
+      .match(event.request)
+      .then(response => response || fetch(event.request)),
+  );
+});
