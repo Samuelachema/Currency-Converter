2758afce3441de978c376cc10f4cd77a66955f2f
Samuel  Achema <achemaku@gmail.com>
2018-07-01 18:53:02 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -10,37 +10,30 @@ var filesToCache = [
 ];
 
 
-self.addEventListener('install', e => {
-  console.log('[ServiceWorker] Install');
-  e.waitUntil(
-    caches.open(cacheName).then(cache => {
-      console.log('[ServiceWorker] Caching app shell');
-      return cache.addAll(filesToCache);
-    }),
-  );
-});
+self.addEventListener('install', e=>{
+    console.log('[ServiceWorker] Install');
+    e.waitUntil(caches.open(cacheName).then(cache=>{
+        console.log('[ServiceWorker] Caching app shell');
+        return cache.addAll(filesToCache);
+    }
+    ), );
+}
+);
 
 
-self.addEventListener('activate', e => {
-  console.log('[ServiceWorker] Activate');
-  e.waitUntil(
-    caches.keys().then(keyList =>
-      Promise.all(
-        keyList.map(key => {
-          if (key !== cacheName) {
+self.addEventListener('activate', e=>{
+    console.log('[ServiceWorker] Activate');
+    e.waitUntil(caches.keys().then(keyList=>Promise.all(keyList.map(key=>{
+        if (key !== cacheName) {
             return caches.delete(key);
-          }
-        }),
-      ),
-    ),
-  );
-});
+        }
+    }
+    ), ), ), );
+}
+);
 
 
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches
-      .match(event.request)
-      .then(response => response || fetch(event.request)),
-  );
-});
+self.addEventListener('fetch', event=>{
+    event.respondWith(caches.match(event.request).then(response=>response || fetch(event.request)), );
+}
+);
